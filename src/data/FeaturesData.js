ab792000dfc66f3898c10921f273e48b376bf105
Lavanya <lavanyarao2217@gmail.com>
2025-08-07 19:54:32 -0400

    feature card data prepped

---
+++ src/data/FeaturesData.js
@@ -0,0 +1,26 @@
+export const FeaturesData = [
+    {
+        id: 1,
+        title: "Experiments",
+        img: "public/images/build_feature.png",
+        description: 'Part of being a "Minecraft Architect" is being an architect! Explore the elements we analyzed by crafting your own builds! Use our tool to plan whatever you like and see it in 3D.',
+        link: "/build-planner",
+        link_name: "Experiment With Your Findings"
+    },
+    {
+        id: 2,
+        title: "Observations",
+        img: "public/images/compare_feature.png",
+        description: 'See how we break down Minecraft builds by element (roofs, windows, etc.) and compare their popular implementations to those in real-life examples.',
+        link: "/build-with-me",
+        link_name: "Analyze Our Worlds"
+    },
+    {
+        id: 3,
+        title: "Observations",
+        img: "public/images/review_feature.png",
+        description: 'Part of analyzing is critiquing and reviewing the resources around us to clarify our understanding. Check out some of our reviews, narrative and otherwise.',
+        link: "/reviews",
+        link_name: "Review Our Reviews"
+    },
+];
