81d5b05a900af99a4cd229c255d1dc32499a2842
Lavanya <lavanyarao2217@gmail.com>
2025-08-11 14:31:48 -0400

    made data and missing files for reviews

---
+++ src/data/MicroReviews.js
@@ -0,0 +1,69 @@
+// Minecraft Micro Reviews and Quick Opinions Data Structure
+export const MicroReviews = {
+  sectionTitle: "Quick Opinions (QOs) and Micro Reviews (MRs)",
+  items: [
+    {
+      id: 1,
+      type: "MR",
+      title: "Build it!",
+      link: "https://builditapp.com/",
+      content: [
+        "Sometimes, I just want to learn from tutorials or templates created by other builders, especially when I reached a block or am looking for a specific style for a specific piece I want in my world--even if I don't have the time or energy to plan out complex buildings. If you ever feel the same way, or in general are looking for a place with a range of build tutorials categorized by build type, style, etc., take a look at Build it! Built it! hosts a lot of free and to-pay very (perhaps even overly) detailed pictoral tutorials/instructions for specific structures fitting a range of styles! You can tell the creators put in a lot of work. They are great for inspiration and practice!"
+      ]
+    },
+    {
+      id: 2,
+      type: "MR",
+      title: "Minecraft Wiki - Village Blueprint",
+      link: "https://minecraft.wiki/w/Village/Structure/Blueprints",
+      content: [
+        "As much as creating my own stuff is great, sometimes I want to learn more about in-game features and implement some of that knowledge into my creations. For the village structures especially, it would be helpful to have a place to look at for structure details and blueprints to better understand the buildings and even recreate them. If you feel the same way, look no further than the Minecraft Wiki's Blueprints page! They go in depth for each item found in villages in each biome, showing layer-by-layer breakdowns of each structure! They also have a lot of useful content about the game across their other pages, so check those out too!"
+      ]
+    },
+    {
+      id: 3,
+      type: "MR",
+      title: "Minecraft Potions - A Visual Guide",
+      link: "https://potioncraft.net/",
+      content: [
+        "Personally, I've always struggled with understanding brewing in Minecraft. Although it's likely been taught somewhere in the tutorials, I've always had issues combining ingredients properly to get what I want, often either because I don't understand the ingredients or don't know the name of a potion I want. If you feel similar, potioncraft.net might be worth a look! They break down each potion by ingredients (and with pictures!), making it easy to connect which ingredients give the effects you want. They also give some helpful links, making referencing information faster and easier than searching online randomly. Overall, I'll be saving this page for later."
+      ]
+    },
+    {
+      id: 4,
+      type: "MR",
+      title: "ChunkBase's Seed-Based Structure Finder",
+      link: "https://www.chunkbase.com/",
+      content: [
+        "I've always wanted to get an idea of how cool the world I am in would be! Whether that be biomes to interesting structures, I normally had no clue where they were an wandered the map aimlessly. Until recently, I didn't know that tools existed to let you see in advance what your world looks like. If you want to see what your world looks like in advance, whether that be the biome spread or locations of special structures, check out Chunkbase.com! Given any seed, they'll show you how your world looks from above, letting you zoom in and out to see just where all your favorite biomes are. It'll be really helpful if you are looking to exploit certain features, like dungeons or ruins for loot!"
+      ]
+    },
+    {
+      id: 5,
+      type: "MR",
+      title: "Minecraft Crafting - Recipe Reminders",
+      link: "https://www.minecraft-crafting.net/",
+      content: [
+        "Depending on the version of Minecraft you play, craft recipes may not always be available. Check out minecraft-crafting.net! It pictorially lists major craft recipes for a handful of weaponry, blocks, utilities, foods, dyes, and more! It also provides links to other helpful resources, letting you have a collectionf crafting knowledge all in once place."
+      ]
+    },
+    {
+      id: 6,
+      type: "QO",
+      title: "Towering Roofs",
+      link: null,
+      content: [
+        "I remember the first time I saw someone post their most recent project online. The first thing that captivated me was the huge roofs on their towers. It felt like an exagerated mediveal-themed day dream for someone learning about that style of build, making the moment all the more awe-filled. The best point of that sort of design is the asthetic. It's beautiful, grand, and well-fitting on just as intricate towers-especially when built into a larger world design. However, I can't find much use for it. When I went to make my own, I realized how small they can really be if your tower isn't incredibly large as its size partly depends on its base. Lighting was difficult too as the area for lighting decreases with each extension of the tower. Nonetheless, if planned right, it is possible to make them a useful elements of the tower. Personally, I will just seal mine off and hope for the best. You can't replace the view a pretty, tall roof on a mythical tower."
+      ]
+    },
+    {
+      id: 7,
+      type: "QO",
+      title: "Birch Wood",
+      link: null,
+      content: [
+        "Birch Wood is a type of wood available in Minecraft's forests, birch forests, dark forests, and meadows. When made into planks, it gets a rather light color close to a white or a cream. Given its different color from other woods in the game, it can be difficult to include in builds that darker asthetic pallets, limiting its use to builds focusing on brighter pallets on some occasions. Personally, for a while I agreed with this as I too like those pallets. Nonetheless, I have some to see the block differently now, and would argue that difference actually gives it value because it acts then as a supplementary block instead of a main pallet color. I like to use it to highlight certain shapes in builds that more uniform pallets, like stairs in a house with an exceptionally dark theme. The contrast gives more color to the space, making the planks live up to its reputation as a less useful wood type but with a twist as it provides a sense of duality while being easy on the eyes."
+      ]
+    }
+  ]
+};
---
+++ src/data/Reviews.js
@@ -0,0 +1,115 @@
+// Minecraft Reviews Data Structure
+export const Reviews = [
+  {
+    id: 1,
+    reviewName: "The Minecraft Construction Handbook: A Place of Inspiration",
+    reviewText: [
+      "In 2013, Mojang released several handbooks to help new players better understand Minecraft's mechanics and the possibilities that came with its style of gameplay, later updating them in 2015. One of the two released was the \"Minecraft Construction Handbook\", a 105-page handbook focused on providing some build ideas for players, such as houses, gardens, and bridges. Overall, the handbook provides a set of instructions to build a few specific basic buildings, which are helpful for players getting started as the images provide clear ideas of what each step should look like, as well as a final version of the model situation in the game, thus removing uncertainty with outcomes, materials, and planning. Additionally, it also hosts several examples from more experienced builders, inspiring new players to create as they like by making them realize the vast creative power the game provides.",
+      
+      "To elaborate on the qualities of instruction sets, the book breaks down builds by major construction. As an example, in the \"Wooden House\", Mojang broke down the build into 8 steps, each relating to a milestone in the build, such as creating the base and starting the roof. The pictures made it easier for me to follow along as I could visually compare my progress with theirs. They also explained why some steps were done, which helped me make sure I didn't do something that I would regret later, like making the base too short and thus cramped later, as they warned. The only downside to their focus on visual milestones is that I didn't have exact measurements. How many blocks wide was their base? 4 by 5 blocks? 10 by 12? The most I could do was guess by counting the blocks in the images, but that also means I could have run into issues later if I had miscounted. Although this may not apply to everyone, trying to complete a build only to find that I made an avoidable mistake would have been disheartening. Overall, the encouraging style of the book's instructions makes it worth a try, in my opinion. Although it does not cover key concepts in building, like planning or color choice, it gives players the confidence to try their best and find out what they like on their own.",
+      
+      "On that note, its examples from other players surely help in the \"find your style\" approach to building. After each instruction set, Mojang provides some example builds from creators in the community, such as an example garden and a massive palace! First, the examples are not all equal. Some are simpler while others are grand, giving a more rounded idea of what building in Minecraft may look like, and thus reassuring that any level of creativity belongs in Minecraft. Personally, I think I can handle creating the example garden made by HalfDemonAnbu, and that palace by Fyreuk left me in awe!",
+      
+      "Although the handbook does not go into detail about how it was built, it does throw in some tips the creator learned while making it. Leaving out the instructions might make the book much better than if they were included. As someone still trying to master building, seeing those grander builds is inspiring! I want to be able to build something like that, but I also question how. I need to plan and think it through first, thus making any result of it my own rather than a copy of someone else's. That feeling is the essence of building: to build as you are! However, I do wish they provided more instructional sets. Inspiration is just as wonderful as guidance is.",
+      
+      "In summation, when it comes to inspiring one's inner creator, this book gets the job done. Its instructions are visually helpful and paced well, making them easier for a new player to follow. Without taking away from those sets, the book's examples also do well to inspire one to build grand and as they see beautiful without hinting at what beautiful can be, fueling players to truly enjoy their creative processes. However, the book still lacks specificity in places where it could have been easily added, which can make the process of following their advice frustrating at times. Nonetheless, the joy the book can bring is well worth it. The pages really do fly by while having fun!"
+    ],
+    images: [
+      {
+        id: 1,
+        src: "images/handbook.png",
+        alt: "handbook",
+        description: "Minecraft Construction Handbook cover",
+        position: "left"
+      },
+      {
+        id: 2,
+        src: "images/woodenhouse.png",
+        alt: "house instructions",
+        description: "Wooden house construction instructions",
+        position: "right"
+      },
+      {
+        id: 3,
+        src: "images/garden.png",
+        alt: "garden example",
+        description: "Example garden build",
+        position: "left"
+      },
+      {
+        id: 4,
+        src: "images/palace.png",
+        alt: "palace example",
+        description: "Example palace build",
+        position: "left"
+      }
+    ],
+    metadata: {
+      tags: ["Minecraft Resources"],
+      date: "03/26/2025",
+      itemData: {
+        title: "Minecraft: Construction Handbook",
+        authors: ["Matthew Needler", "Phil Southam"],
+        publicationDate: "August 26, 2014, updated July 28, 2015",
+        pageCount: 105,
+        forms: ["Hardcover", "Paperback", "Virtual/Online"]
+      },
+      links: {
+        furtherResources: "https://minecraft.fandom.com/wiki/Minecraft_Books",
+        credits: "https://archive.org/details/MCCSNHandbook/mode/2up"
+      }
+    }
+  },
+  {
+    id: 2,
+    reviewName: "The Plains Biome House #5 From the Perspective of a \"Home Buyer\": A Better Lodging Than Most, But Nonetheless Disappointing",
+    reviewText: [
+      "Hello all! Today, I continued my search for lodging in the world I call my home! After some searching, I was able to contact a wonderful salesman, Jeffrey. Shout out to Jeffrey for his many tips and vast knowledge. After some discussion, Jeffrey recommended I take a look at \"House #5\" in the Plains Biome! Most villages seemed rather full, but he was able to find a vacant one nearby for me to view! From the outside, the house has two floors, a balcony on the second floor, and many windows on the first floor. Given it is in the Plains Biome, it has grassy surroundings, and weather that was not terribly hot or cold, seeing at most the occasional rain falls. Exterior-wise, the color picks are beautiful--not too bold but also easy on the eyes. The interior left me disappointed, in contrast.",
+      
+      "To elaborate, Jeffrey took the time to visit the house with me, pointing out many of its features. Regarding the first floor, I was excited to find that the windows let in a lot of natural light. However, their positioning made it so most of the light would come in during the evenings, which was slightly disappointing, as having consistent lighting would have made it much cozier. Jeffrey assured me nonetheless that the rooms had ample torches, so I would not have to worry about any hostile mobs attacking my home, as they would be well-lit regardless of the time of day. Furniture wise, I did appreciate the table and couch. Although I wish they did provide a larger table, I can also easily create one myself. The extra seating would let me have friends over. Oh, the get-togethers we could have! The room itself is also decently spacious (a bit larger than 5 by 5 blocks), which is a plus, as other houses I have seen have been a bit cramped and only fit a bed and a single chest for my belongings (normally 3 by 3 blocks). The room was also of decent height (4 blocks), so I could move around with ease. The only thing that irked me was the presence of a bed. Given that the building had two floors, I was expecting this sort of furniture to be upstairs.",
+      
+      "Now, cue my disappointment. The second \"floor\" was really a narrow pathway leading to the balcony. The space is surely enough for those who would use the balcony mainly to fight off hostile mobs safely, as there was enough space on the balcony (2-block width instead of 1 like the second floor) to move around freely to shoot creatures with arrows. However, I was hoping for more space as I want to decorate their lodging. Where will my gardens, pictures, and chests go? Furthermore, people being able to look through the windows on the first floor and see me sleeping is not particularly pleasant to me. A thing to note is that the floor is expandable if one has the trees nearby to gather wood to create and place more planks. All one has to do is find a nearby tree (which may be easy or difficult depending on where in the plaines biome one is), take an axe to its side, and collect the wood to craft planks. That is something I kept in mind, as at least I could transform the space rather than being stuck with this lackluster floor plan.",
+      
+      "Overall, House #5 in the Plains Biome is a decent lodging. The surroundings are nice and decently orderly, the exterior is pleasing (to my eye at least), and the first floor is well lit, spacious, and does come with some nice furniture. Although I was disappointed with the small size of the second floor, the expandability of it does make up for it if one is willing to put the time into it. Ultimately, I decided to go with the house, so my house-buying adventures end here! Off to find some wood now to fix that poor second floor."
+    ],
+    images: [
+      {
+        id: 5,
+        src: "images/villagehouse5.png",
+        alt: "Village House #5",
+        description: "Plains Biome House #5 exterior view",
+        position: "left"
+      },
+      {
+        id: 6,
+        src: "images/h5Interiorfirstfloor.png",
+        alt: "first floor",
+        description: "House #5 first floor interior",
+        position: "right"
+      },
+      {
+        id: 7,
+        src: "images/h5Interiorsecondfloor.png",
+        alt: "second floor",
+        description: "House #5 second floor interior",
+        position: "left"
+      }
+    ],
+    metadata: {
+      tags: ["Village Structures", "In-Game Structures"],
+      date: "03/26/2025",
+      itemData: {
+        location: "Plains Biome",
+        structureCollection: "Village",
+        residentOccupation: "Null",
+        floorCount: 2,
+        materials: ["Cobblestone", "Oak wood planks", "doors", "stairs", "fences"],
+        lightingSource: "torches"
+      },
+      links: {
+        furtherResources: "https://minecraft.wiki/w/Village/Structure/Blueprints#Medium_House_3",
+        credits: "https://www.youtube.com/watch?v=gHpVoBPfHZ0"
+      }
+    }
+  }
+];
